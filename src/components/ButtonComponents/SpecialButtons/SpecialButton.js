eefacc5f747de8017825c60d7669704ed884189f
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-11 01:21:11 -0500

    some styling work

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -4,9 +4,11 @@ export const SpecialButton = props => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className = "special__buttons">
-        {props.item}
-      </button>
+      
+        <button className="special__buttons">
+          {props.item}
+        </button>
+      
     </>
   );
 };
